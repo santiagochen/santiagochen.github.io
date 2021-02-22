440c2c9c68d1d0a3c2ea85eb598512d7b2728773
santiagochen <545438641@qq.com>
2021-02-22 19:38:53 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,26 +27,23 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-60fe6b82efad0cfea747.js"
+    "url": "webpack-runtime-4a4b500b763b1b9251e6.js"
   },
   {
-    "url": "d2956fd2-9247bfcd1df904489073.js"
+    "url": "framework-2d6b7d2e2be378715b78.js"
   },
   {
-    "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-03021c5c5bb73c91366b.js"
-  },
-  {
-    "url": "app-fe928dec10b091a537d8.js"
+    "url": "app-e13beda5f7ef4feecbfa.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ba91561f3b1390ea3c5094d9e7e01c46"
+    "revision": "15814c8a411d25970f36771589705ba5"
   },
   {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-549cf0206391024ff896.js"
+    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-780d0321bb4481125aff.js"
   },
   {
-    "url": "polyfill-a6321ebebaec8f53fc7a.js"
+    "url": "polyfill-8323541ae364849595b4.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -142,7 +139,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-fe928dec10b091a537d8.js`))) {
+  if (!resources || !(await caches.match(`/app-e13beda5f7ef4feecbfa.js`))) {
     return await fetch(event.request)
   }
 
